4d49a3d1267a7e2a74719f946630f0252b73b5db
tonihurtado <tonihurtado13@gmail.com>
2018-08-03 11:37:31 +0200

    Test protractor use

---
+++ conf.js
@@ -0,0 +1,13 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  multiCapabilities: [
+    {
+      browserName: 'firefox'
+    },
+    {
+      browserName: 'chrome'
+    }
+  ]
+};
